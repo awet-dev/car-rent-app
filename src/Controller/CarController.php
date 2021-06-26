5611c0c7dd017fd97d84308eef493bf1bb68a3af
awet tesfay <whitezscope@gmail.com>
2021-06-26 23:30:57 +0200

    style the carousel for the car in the market

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -13,7 +13,7 @@ class CarController extends AbstractController
     public function index(CarRepository $carRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'cars' => $carRepository->findAll(),
+            'cars' => $this->carsArrayGrouping($carRepository->findAll()),
         ]);
     }
 
@@ -23,5 +23,16 @@ class CarController extends AbstractController
         return $this->render('home/detail.html.twig', [
             'car' => $carRepository->find($id),
         ]);
+    }
+
+    public function carsArrayGrouping($cars): array
+    {
+        $i = 0;
+        $array_cars = [];
+        while ($i < count($cars)) {
+            array_push($array_cars, array_slice($cars, $i, 3));
+            $i += 3;
+        }
+        return $array_cars;
     }
 }
